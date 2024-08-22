1f7481ce598e44ee1be3b13335f539076aa03ff3
mhd-ashmal38 <mhdashmal38@gmail.com>
2024-08-22 12:16:16 +0530

    feat: Create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AddTask from './components/AddTask';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-5xl font-bold text-center'>Task Manager</h1>
+      <AddTask/>
     </div>
   );
 }
